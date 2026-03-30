ce958fcb13b86e56caaead7fa7fa29bd2e274b5d
Kai Garrod <kaigarrod@gmail.com>
2026-03-30 15:58:36 +0100

    Create PUT method for updating film titles via id

--- app.py
+++ app.py
@@ -41,16 +41,36 @@ def post_films():
 @app.route("/api/films/<id>", methods=["GET"])
 def get_films_id(id):
     try:
-        ObjectId(id)
+        obj_id = ObjectId(id)
     except Exception:
         return jsonify({"error": "invalid id"}), 400
     result = [serialize_film(film)
-              for film in films.find({"_id": ObjectId(id)})]
+              for film in films.find({"_id": obj_id})]
 
     if len(result) == 0:
         return (jsonify({"error": "film not found"})), 404
     return jsonify({"data": result}), 200
 
+
+@app.route("/api/films/<id>", methods=["PUT"])
+def put_films_id(id):
+    try:
+        obj_id = ObjectId(id)
+    except Exception:
+        return jsonify({"error": "invalid id"}), 400
+    if not request.json or not "title" in request.json or not request.json["title"].strip():
+        return jsonify({"error": "invalid title"}), 400
+
+    filter = {"_id": obj_id}
+    update = {"$set": {"title": request.json["title"]}}
+
+    films.update_one(
+        filter=filter, update=update)
+
+    result = [serialize_film(film)
+              for film in films.find({"_id": obj_id})]
+
+    return jsonify({"data": result}), 200
 
 
 app.run()
