430c4473c8ae3983d1d64cea250da699b495cbd5
Kai Garrod <kaigarrod@gmail.com>
2026-03-30 15:35:42 +0100

    Add first route for GET all films in db

--- app.py
+++ app.py
@@ -10,5 +10,17 @@ films = db.get_collection("films")
 app = Flask(__name__)
 
 
+def serialize_film(film):
+    return {
+        "id": str(film["_id"]),
+        "title": film["title"]
+    }
+
+
+@app.route("/api/films", methods=["GET"])
+def get_films():
+    result = [serialize_film(film) for film in films.find({})]
+    return jsonify({"data": result}), 200
+
 
 app.run()
